6001f926277bf61a033fcc3e7b2daf1477d24323
panda <82871987@qq.com>
2018-01-01 16:28:35 +0800

    文件上传

--- cloudStoreService/WebContent/sendmess.jsp
+++ cloudStoreService/WebContent/sendmess.jsp
@@ -1,6 +1,10 @@
 <%@ page language="java" contentType="text/html; charset=utf-8"
     pageEncoding="utf-8"%>
-
+<%
+	String path = request.getContextPath();
+	String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort()
+			+ path + "/";
+%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -8,10 +12,21 @@
 <title>Insert title here</title>
 </head>
 <body>
-<%
+<!-- <form action="fileDownLoadTest" enctype="multipart/form-data" method="post">    
+上传用户：<input type="text" name="username"><br/>
+          上传文件1：<input type="file" name="file1"><br/>
+          上传文件2：<input type="file" name="file2"><br/>
+          <input type="submit" value="提交">
+</form> -->
+	<form action="fileDownLoadTest" enctype="multipart/form-data" method="post">
+		上传用户：<input type="text" name="username"><br>
+		上传文件1：<input type="file" name="file1"><br>
+		 上传文件2：<input type="file" name="file2"><br>
+		<input type="submit" value="提交">
+	</form>
+	<img alt="图片" src="<%=basePath%>/img/timg.jpg">
 
-	//com.cloudstore.tools.javaBean.SendMessage.sendMessage("18875686859", "15151");
 
-%>
+
 </body>
 </html>
---
+++ cloudStoreService/src/com/cloudstore/servlet/Test.java
@@ -0,0 +1,58 @@
+package com.cloudstore.servlet;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Test
+ */
+@WebServlet(name = "test", urlPatterns = { "/test" })
+public class Test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Test() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		System.out.println("getCharacterEncoding-->"+request.getCharacterEncoding());
+//		System.out.println("getContentLength-->"+request.getContentLength());
+//		System.out.println("getContentType-->"+request.getContentType());
+//		System.out.println("getLocalAddr-->"+request.getLocalAddr());
+//		System.out.println("getLocale-->"+request.getLocale());
+//		System.out.println("getLocalName-->"+request.getLocalName());
+//		System.out.println("getLocalPort-->"+request.getLocalPort());
+//		System.out.println("getProtocol-->"+request.getProtocol());
+//		System.out.println("getRemoteAddr-->"+request.getRemoteAddr());
+//		System.out.println("getRemoteHost-->"+request.getRemoteHost());
+//		System.out.println("getRemotePort-->"+request.getRemotePort());
+//		System.out.println("getScheme-->"+request.getScheme());
+//		System.out.println("getContextPath-->"+request.getContextPath());
+//		System.out.println("getRequestURI-->"+request.getRequestURI());
+//		System.out.println("getRequestURL-->"+request.getRequestURL());
+//		System.out.println("getPathInfo-->"+request.getPathInfo());
+//		System.out.println("getPathTranslated-->"+request.getPathTranslated());
+//		System.out.println("getQueryString-->"+request.getQueryString());
+//		System.out.println("getRemoteUser-->"+request.getRemoteUser());
+		
+	
+		
+		
+		
+	}
+
+}
